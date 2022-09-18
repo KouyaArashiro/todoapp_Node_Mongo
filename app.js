86e9a3fa908f76eeda325fb20d9f3b5522397fc2
KouyaArashiro <arashiro.code@gmail.com>
2022-09-19 06:00:48 +0900

    conf routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const taskRoutes = require("./routes/tasks");
 const PORT = 5001;
 
+app.use("/api/v1/tasks", taskRoutes);
 app.listen(PORT, console.log('server started'));
