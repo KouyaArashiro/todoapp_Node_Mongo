8897571c7edc7f9709f379baeb5a80f1af6b0063
KouyaArashiro <arashiro.code@gmail.com>
2022-09-21 21:25:24 +0900

    connectDB

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const taskRoutes = require("./routes/tasks");
+const connectDB = require("./db/connect");
 const PORT = 5001;
 
+//ルーティング設計 
 app.use("/api/v1/tasks", taskRoutes);
-app.listen(PORT, console.log('server started'));
+
+//DBと接続
+const start = async () => {
+    try {
+        await connectDB(
+            "mongodb+srv://kouya:test@cluster0.8zylzhp.mongodb.net/?retryWrites=true&w=majority"
+            );
+            app.listen(PORT, console.log("server started"));
+    } catch (err){
+        console.log(err);
+    }
+}
+
+start();
