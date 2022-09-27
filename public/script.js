2be6dd53e9e43ad309c3becdde9d9bfdfb40cc04
KouyaArashiro <a.kouya2001@gmail.com>
2022-09-27 19:20:50 +0900

    use axios

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log(axios);
+// /api/v1/tasksからタスクを取得
+const showTasks  = async () => {
+    try {
+        const { data: tasks } = await axios.get("/api/v1/tasks");
+        console.log(tasks);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+showTasks();
