e5d084dd8d00b75125f4cc67ea91b034a63acafe
KouyaArashiro <a.kouya2001@gmail.com>
2022-10-05 19:41:32 +0900

    task empty handling

--- public/script.js
+++ public/script.js
@@ -8,6 +8,13 @@ const showTasks  = async () => {
         const { data: tasks } = await axios.get("/api/v1/tasks");
         // console.log(tasks);
 
+        //タスクが1つも無い時
+        // console.log(tasks.length);
+        if(tasks.length < 1) {
+            tasksDOM.innerHTML = `<h5 class="empty-list">タスクがありません</h5>`
+            return;
+        }
+
         //タスクを出力
         const allTasks = tasks.map((task) => {
             const { completed, _id, name } = task;
