8ee011ee2fad1580527f6a8ba81ed688016cb419
KouyaArashiro <a.kouya2001@gmail.com>
2022-10-05 20:39:56 +0900

    add showTask function at editTask.js

--- public/editTask.js
+++ public/editTask.js
@@ -1,3 +1,19 @@
+const taskDOM = document.querySelector(".task-edit-id");
+const taskNameDOM = document.querySelector(".task-edit-name");
+
 const params = window.location.search;
 const id = new URLSearchParams(params).get("id");
 console.log(id);
+
+const showTask = async () => {
+    try {
+        const { data: task } = await axios.get(`/api/v1/tasks/${id}`);
+        const { _id, completed, name } = task;
+        taskDOM.textContent = _id;
+        taskNameDOM.value = name;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+showTask();
