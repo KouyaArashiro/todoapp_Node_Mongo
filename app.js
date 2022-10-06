ea96f66f55e79a3fa2239a689bc0a934d1334262
KouyaArashiro <a.kouya2001@gmail.com>
2022-10-06 11:04:47 +0900

    create mongoURL

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use("/api/v1/tasks", taskRoutes);
 //DBと接続
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URL);
+        await connectDB(process.env.MONGO_HEROKU_URL || process.env.MONGO_URL);
             app.listen(process.env.PORT || PORT, console.log("server started"));
     } catch (err){
         console.log(err);
